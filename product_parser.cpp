018887f8a32bac2c11d5e5c4103aa91eea07eaa6
Rishee Venkatesh <risheeve@usc.edu>
2024-02-12 20:58:29 -0800

    modified products

--- product_parser.cpp
+++ product_parser.cpp
@@ -130,7 +130,7 @@ std::string ProductBookParser::categoryID()
  */
 Product* ProductBookParser::makeProduct()
 {
-  return new Book(prodName_, price_, qty_, isbn_, author_);
+  return new Book("book", prodName_, price_, qty_, isbn_, author_);
 
 }
 
@@ -185,7 +185,7 @@ std::string ProductClothingParser::categoryID()
  */
 Product* ProductClothingParser::makeProduct()
 {
-  return new Clothing(prodName_, price_, qty_, size_, brand_);
+  return new Clothing("clothing", prodName_, price_, qty_, size_, brand_);
 
 
 }
@@ -246,6 +246,6 @@ std::string ProductMovieParser::categoryID()
  */
 Product* ProductMovieParser::makeProduct()
 {
-  return new Movie(prodName_, price_, qty_, genre_, rating_);
+  return new Movie("movie", prodName_, price_, qty_, genre_, rating_);
 
 }
